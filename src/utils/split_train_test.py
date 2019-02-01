9647b2ae6ee26308e7715f065f432cd6ab4f5455
Amrita Panesar <09panesara@gmail.com>
2019-02-01 14:36:28 +0000

    Data files update, split_train_test

--- src/utils/split_train_test.py
+++ src/utils/split_train_test.py
@@ -1,9 +1,9 @@
-import numpy as np
 from __future__ import division
+import numpy as np
 import pandas as pd
 import random
 
-no_videos = 563
+NO_OF_VIDEOS = 563
 
 
 '''
@@ -27,7 +27,7 @@ def split(df, train_split=60, val_split=20, test_split=20):
     validation_df = pd.DataFrame(columns=columns)
     test_df = pd.DataFrame(columns=columns)
 
-    df_len = no_videos
+    df_len = NO_OF_VIDEOS
     train_val_test_split_total = train_split+val_split+test_split
     train_split = int(train_split/train_val_test_split_total * df_len)
     val_split = int(val_split/train_val_test_split_total * df_len)
@@ -80,9 +80,37 @@ def split(df, train_split=60, val_split=20, test_split=20):
     test_df.to_hdf('../data/test_data.h5', key='df', mode='w')
 
 
-def cross_validation(k, train_split=80, test_split=20):
 
+def stratified_split(X, y, folds=5):
+    ''' Multiclass stratified split '''
+    obs, classes = y.shape
+    dist = y.sum(axis=0).astype('float')
+    dist /= dist.sum()
+    index_list = []
+    fold_dist = np.zeros((folds, classes), dtype='float')
+    for _ in range(folds):
+        index_list.append([])
+    for i in range(obs):
+        if i < folds:
+            target_fold = i
+        else:
+            normed_folds = fold_dist.T / fold_dist.sum(axis=1)
+            how_off = normed_folds.T - dist
+            target_fold = np.argmin(np.dot((y[i] - .5).reshape(1, -1), how_off.T))
+        fold_dist[target_fold] += y[i]
+        index_list[target_fold].append(i)
 
+    print('Folds are')
+    folds = [X[index] for index in index_list]
+    classes = [y[index] for index in index_list]
+    return folds, classes
 
-
-
+if __name__ == '__main__':
+    # X = np.array([[]])
+    y = np.array([['sad', '29f'], ['sad', '29f'], ['sad', '28m'], ['ang', '28m'], ['ang', '01m'], ['unt', '01m'], ['fea', '01m'], ['fea', '28m'], ['fea', '23m']])
+    emotions = {'ang': 0, 'fea': 1, 'hap': 2, 'neu': 3, 'sad': 4, 'unt': 5}
+    y = np.array([np.array([emotions[labels[0]], int(labels[1][:-1])]) for labels in y])
+    index_list = stratified_split(y, 2)
+    print('Folds are')
+    folds = [y[index] for index in index_list]
+    print(folds)
