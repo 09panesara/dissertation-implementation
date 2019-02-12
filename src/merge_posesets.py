d58f048a6bb7c656e938595c8da379f2a92a6ba2
Amrita Panesar <09panesara@gmail.com>
2019-02-12 14:47:58 +0000

    Merged centers from different emotion categories

--- src/merge_posesets.py
+++ src/merge_posesets.py
@@ -2,54 +2,71 @@
 import os
 import numpy as np
 import random
+from sklearn import preprocessing
 
 emotions = emotions = ['ang', 'fea', 'hap', 'sad', 'unt']
 def get_centers(dir):
-    cluster_centers = [np.load(dir+'/cluster_' + emotion + '.npz', encoding='latin1') for emotion in emotions if os.path.isdir(dir+'/cluster_' + emotion + '.npz')]
+    print('Loading centers...')
+    cluster_centers = [np.load(dir+'/clusters_' + emotion + '.npz', encoding='latin1') for emotion in emotions if os.path.isfile(dir+'/clusters_' + emotion + '.npz')]
     cluster_centers = [clusters['predicted_centers'] for clusters in cluster_centers]
     # Flatten
     cluster_centers = [center for emotion_center_set in cluster_centers for center in emotion_center_set]
-    return cluster_centers
+
+    return _convert_np_arr_to_set(cluster_centers)
 
 def _euclid_distance(c1, c2):
     c1 = np.array(c1)
     c2 = np.array(c2)
-    return np.linal.norm(c1-c2)
+    return np.linalg.norm(c1-c2)
+
+def _convert_np_arr_to_set(arr):
+    arr = set([tuple(y) for y in arr])
+    arr = [list(tpl) for tpl in arr]
+    return arr
 
 #TODO test
 def merge_centers(centers, thresh):
     ''' Iteratively merge centroids with a distance less than thresh '''
     old_centers = set([])
-    new_centers = set(centers)
-
+    new_centers = _convert_np_arr_to_set(centers)
+    print('Merging with threshold: ' + str(thresh))
 
     while old_centers != new_centers:
         old_centers = new_centers
         no_centers = len(old_centers)
         # choose center at random and eliminate other centroid at < thresh away
         removed = False
-        while (not removed):
-            center = random.randint(1, len(old_centers))
+        picked = []
+        while not(removed):
+            if len(picked) == no_centers:
+                break
+            center = random.randint(1, no_centers)
+            picked.append(center)
             distances = [i for i, c in enumerate(old_centers) if c != center and _euclid_distance(c, center) < thresh]
             if len(distances) > 0:
                 rmv_index = random.choice(distances)
                 new_centers = [center for center in old_centers if center != old_centers[rmv_index]]
-            else:
                 removed = True
+
     return new_centers
 
 
 
 
+def _calc_dist(centers):
+    dist = []
+    for c1 in centers:
+        for c2 in centers:
+            if (c1 != c2):
+                dist.append(_euclid_distance(c1, c2))
+    print(np.mean(dist))
 
 
-
-def find_thresh(posesets):
+def find_thresh():
     dir = '../data/clusters'
     emotion_centers = get_centers(dir)
-
-
-    thresholds = [3.5, 4, 4.5, 5, 5.5, 6]
+    thresholds = [i for i in range(500, 10000, 500)] # pick thresh = 6500
+    # thresholds = [3.5, 4, 4.5, 5, 5.5, 6]
     dict_size = []
     for threshold in thresholds:
         merged = merge_centers(emotion_centers, threshold)
@@ -58,6 +75,18 @@ def find_thresh(posesets):
     with open(file_path, 'w') as file:
         file.write('thresh poseset_size\n')
         for i in range(len(thresholds)):
-            file.write(str(thresholds[i]) + ' ' + str(len(merged[i])) + "\n")
+            file.write(str(thresholds[i]) + ' ' + str(dict_size[i]) + "\n")
+
+if __name__ == '__main__':
+    dir = '../data/clusters'
+    emotion_centers = get_centers(dir)
+    # emotion_centers = preprocessing.scale(emotion_centers)
+    thresh = 6500
+    new_centers = merge_centers(emotion_centers, thresh=thresh)
+    print('Saving merged centers...')
+    np.savez_compressed('../data/merged_centers.npz', merged_centers=new_centers, thresh=thresh)
+    print('Done.')
+
+
 
 
--- src/obtain_reference_poseset.py
+++ src/obtain_reference_poseset.py
@@ -5,38 +5,73 @@ import pandas as pd
 import os
 
 def flatten_by_frame(df):
+    '''
+    :param df: dataframe for particular emotion, containing one row per video
+    :return: Flattened dataframe containing 1 row per frame
+    '''
+    cols_to_ignore = ['emotion', 'intensity', 'subject', 'action']
+    df = df.drop(cols_to_ignore, axis=1)
     columns = df.columns.values
-    for column_name in columns:
-        col = df[[column_name]]
+    split_df = pd.DataFrame(columns=columns)
 
-    return
 
-def generate_lexicon(emotion, train):
+    for i, row in df.iterrows():
+        timestep = row['timestep_btwn_frame']
+        no_frames = len(row['rise_sink'])
+        timestep = [timestep for i in range(no_frames)]
+        row['timestep_btwn_frame'] = timestep
+        arr = np.array(row)
+        arr = list(zip(*arr))
+        arr = [np.array(row) for row in arr]
+        df_temp = pd.DataFrame(arr, columns=columns)
+        assert(len(df_temp.columns.values)==len(split_df.columns.values))
+        split_df = split_df.append(df_temp)
+    return split_df
+
+def generate_lexicon(emotion, train, override_existing_clusters=False, visualise_clusters=True):
     '''
     Generates lexicon for reference poseset per emotion category
     :return:
     '''
-
     # obtain action sequences for emotion
-    no_frames = 130
-    X =[]
+    no_frames = len(train)
 
-    # Carry out k-medians
-    k_medians = kmedians(10, no_frames)
-    k_medians = k_medians.fit(X)
-    predicted_clusters = k_medians._get_clusters(X)
-    kmedians._visualise_clusters(predicted_clusters)
+    # Check if need to carry out k-medians or clusters exist already
+    file_path = '../data/clusters/clusters_' + emotion + '.npz'
+    k_medians = kmedians(8, no_frames, max_iter=50)
+    if not os.path.isfile(file_path) or override_existing_clusters:
+        # Carry out k-medians
+        k_medians = k_medians.fit(train)
+        predicted_clusters = k_medians._get_clusters(train)
+        print('Saving clusters...')
+        np.savez_compressed(file_path, predicted_clusters=predicted_clusters, predicted_centers=k_medians.cluster_centers_)
+        print('Done.')
+    else:
+        clusters_data = np.load(file_path, encoding='latin1')
+        predicted_clusters = clusters_data['predicted_clusters']
+        predicted_centers = clusters_data['predicted_centers']
+
+    if visualise_clusters:
+        print('Visualising clusters...')
+        k_medians._visualise_clusters(clusters=predicted_clusters, plot_name='kmedians_' + emotion + '.png')
 
 
 
-emotions = ['ang', 'fea', 'hap', 'neu', 'sad', 'unt']
-
+emotions = ['ang', 'fea', 'hap', 'sad', 'unt']
+# neu = empty so ignore
 LMA_train, LMA_test = data_utils.get_train_test_set()
-# LMA_train = LMA_train.drop(['subject'], axis=1)
-#
-#
-# for emotion in emotions:
-#     df = LMA_train.loc[LMA_train['emotion'] == emotion]
-#     df = df.drop(['emotion'], axis=1)
-#     generate_lexicon(emotion, np.array(df))
+
+
+for emotion in emotions:
+    print('Finding clusters for emotion ' + emotion)
+    df_path = '../data/train_' + emotion + '.h5'
+    if not os.path.isfile(df_path):
+        df = LMA_train.loc[LMA_train['emotion'] == emotion]
+        df = flatten_by_frame(df)
+        # Write pandas dataframe to compressed h5.py file
+        df.to_hdf(df_path, key='df', mode='w')
+    else:
+        df = pd.read_hdf(df_path)
+    df_arr = np.array(df)
+    generate_lexicon(emotion, df_arr)
 
